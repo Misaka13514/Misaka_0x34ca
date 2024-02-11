4fa65bfc4b534c1d8ebf5bf5644ba9de6056dd4c
Misaka13514 <Misaka13514@gmail.com>
2024-02-11 15:17:17 +0800

    atri.tk -> apeiria.net

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-const anchors = document.querySelectorAll('a[href^="http"]:not([href*="atri.tk"])')
+const anchors = document.querySelectorAll('a[href^="http"]:not([href*="apeiria.net"])')
 anchors.forEach(anchor => anchor.setAttribute('target', '_blank'))
 anchors.forEach(anchor => anchor.setAttribute('rel', 'external noopenner nofollow'))
